f172b28c7ac4a24cbfe6bb01cc5ef2dc3327913d
Michelle <michelle@thewcl.com>
2025-07-01 21:37:55 -0500

    stretch

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-print("Enter the first number:")
-num1 = float(input())
-print("Enter the second number:")
-num2 = float(input())
-print("Enter an operation (+, -, *, /):")
-operator = input()
+#print("Enter the first number:")
+#num1 = float(input())
+#print("Enter the second number:")
+#num2 = float(input())
+#print("Enter an operation (+, -, *, /):")
+#operator = input()
 
-if operator == "+":
-    print("The result is ", num1 + num2)
-elif operator == "-":
-    print("The result is ", num1 - num2)
-elif operator == "*":
-    print("The result is ", num1 * num2)
-elif operator == "/":
-    if num2 == 0:
-        print("Oops! You can't divide by zero.")
+print("Enter a math expression:")
+expression = input()
+parts = expression.strip().split()
+
+if len(parts) != 3:
+    print("Invalid expression.")
+
+else:
+    num1_str, operator, num2_str = parts
+    num1 = float(num1_str)
+    num2 = float(num2_str)
+
+    if operator == "+":
+        print("Tada! You get ", num1 + num2)
+    elif operator == "-":
+        print("Tada! You get ", num1 - num2)
+    elif operator == "*":
+        print("Tada! You get ", num1 * num2)
+    elif operator == "/":
+        if num2 == 0:
+            print("Oops! You can't divide by zero.")
+        else:
+            print("Tada! You get ", num1 / num2)
     else:
-        print("The result is ", num1 / num2)
+        print("Sorry, I don’t know how to do that operation.")
